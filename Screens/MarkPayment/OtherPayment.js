127f6aecc363c97dd62e61209405a039a60822a1
sidath <sidaath@gmail.com>
2022-04-05 17:50:18 +0530

    datepicker

--- Screens/MarkPayment/OtherPayment.js
+++ Screens/MarkPayment/OtherPayment.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { Button, Card, TextInput } from 'react-native-paper'
 import { makeOtherPayment } from '../../Model/Payment'
 
@@ -7,7 +8,10 @@ export default function OtherPayment({ route, navigation }) {
 
     const { bill } = route.params
 
-    const [date, setDate] = React.useState('')
+    const [date, setDate] = React.useState(new Date())
+    const [mode, setMode] = React.useState('date');
+    const [show, setShow] = React.useState(false);
+
     const [amount, setAmount] = React.useState('')
     const [method, setMethod] = React.useState('')
 
@@ -15,34 +19,51 @@ export default function OtherPayment({ route, navigation }) {
     const [amountError, setAmountError] = React.useState(false)
     const [methodError, setMethodError] = React.useState(false)
 
-    
-    function resetErrors(){
+
+    function resetErrors() {
         setDateError(false)
         setMethodError(false)
         setAmountError(false)
     }
 
-    const save = () =>{
+    const save = () => {
         resetErrors()
         let error = false
-        
-        if(date===''){
+
+        if (date === '') {
             error = true
             setDateError(true)
         }
 
-        if(amount===''){
+        if (amount === '') {
             error = true
             setAmountError(true)
         }
-        if(method===''){
+        if (method === '') {
             error = true
             setMethodError(true)
         }
-        if(error) return
+        if (error) return
 
-        makeOtherPayment(bill.name, date, amount, method)
+        makeOtherPayment(bill.name, date.toLocaleDateString(), amount, method)
     }
+
+
+    //date functions
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
 
 
 
@@ -54,22 +75,34 @@ export default function OtherPayment({ route, navigation }) {
             <Card>
                 <Card.Title title='Payment Details' />
                 <Card.Content>
-                    <TextInput
+                    {/* <TextInput
                         mode='outlined'
                         value={date}
                         placeholder="date of payment"
                         label="Date"
                         error={dateError}
-                        onChangeText={(text)=>setDate(text)}
+                        onChangeText={(text) => setDate(text)}
+                    /> */}
+                    <TextInput 
+                        mode='flat'
+                        value={date.toLocaleDateString()}
+                        label='Selected Date'
                     />
-
+                    {show && <DateTimePicker 
+                        testID='dateTimePicker'
+                        value={date}
+                        mode='date'
+                        onChange={onChange}
+                    />}
+                    <Button onPress={()=>{showDatepicker()}}>Open Date Picker</Button>
                     <TextInput
                         mode='outlined'
                         value={amount}
                         placeholder="Amount"
                         label="Amount"
                         error={amountError}
-                        onChangeText={(text)=>setAmount(text)}
+                        keyboardType='numeric'
+                        onChangeText={(text) => setAmount(text)}
                     />
 
                     <TextInput
@@ -78,13 +111,13 @@ export default function OtherPayment({ route, navigation }) {
                         placeholder="Cash, Card etc"
                         label="Payment Method"
                         error={methodError}
-                        onChangeText={(text)=>setMethod(text)}
+                        onChangeText={(text) => setMethod(text)}
                     />
 
                 </Card.Content>
             </Card>
 
-            <Button onPress={()=>{save()}}>Save</Button>
+            <Button onPress={() => { save() }}>Save</Button>
         </View>
     )
 }
