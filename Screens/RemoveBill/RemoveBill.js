f6b5ab9327aadbdeb3c641b127ee7ed9e6e4d888
sidath <sidaath@gmail.com>
2022-04-07 20:20:16 +0530

    confirmation dialog on delete

--- Screens/RemoveBill/RemoveBill.js
+++ Screens/RemoveBill/RemoveBill.js
@@ -1,57 +1,69 @@
 import React from 'react'
-import {ScrollView, ActivityIndicator, StyleSheet} from 'react-native'
-import { Button, Card, Paragraph } from 'react-native-paper'
+import { ScrollView, ActivityIndicator, StyleSheet } from 'react-native'
+import { Button, Card, Dialog, Paragraph, Portal } from 'react-native-paper'
 import { removeBill } from '../../Model/BillModel'
 import { getRecordNumbers } from '../../Model/Payment'
 
-class RemoveBill extends React.Component{
-    constructor(props){
+class RemoveBill extends React.Component {
+    constructor(props) {
         super(props)
-        this.state = {dataLoaded : false}
+        this.state = { dataLoaded: false, showDialog: false, removeName : '' }
     }
 
-    async componentDidMount(){
-        try{
+    async componentDidMount() {
+        try {
             const paymentRecords = await getRecordNumbers()
-            this.setState({paymentRecords : paymentRecords, dataLoaded : true})
-        }catch(e){
+            this.setState({ paymentRecords: paymentRecords, dataLoaded: true })
+        } catch (e) {
             console.log("RemoveBill.js, L18 ERROR : ", e)
         }
     }
 
-    render(){
-        const {reload} = this.props.route.params
+    render() {
+        const { reload } = this.props.route.params
 
-        const handleDelete = async (billName)=>{
-            removeBill(billName).then((res)=>{
-                if(res){
+        const handleDelete = async () => {
+            removeBill(this.state.removeName).then((res) => {
+                if (res) {
                     reload()
                     this.props.navigation.goBack()
                 }
             })
         }
 
-        if(!this.state.dataLoaded){
-            return(
-                <ActivityIndicator size='large'/>
+        if (!this.state.dataLoaded) {
+            return (
+                <ActivityIndicator size='large' />
             )
         }
 
-        return(
+        return (
             <ScrollView>
-                {this.state.paymentRecords.map((record)=>{
-                    return(
+                {this.state.paymentRecords.map((record) => {
+                    return (
                         <Card key={record.billName} style={styles.card}>
                             <Card.Title title={record.billName} />
                             <Card.Content>
                                 <Paragraph>{`Total records : ${record.total}`}</Paragraph>
                             </Card.Content>
                             <Card.Actions>
-                                <Button onPress={()=>handleDelete(record.billName)}>Delete</Button>
+                                <Button onPress={() => this.setState({showDialog : true, removeName:record.billName})}>Delete</Button>
                             </Card.Actions>
                         </Card>
                     )
                 })}
+                <Portal>
+                    <Dialog visible={this.state.showDialog}>
+                        <Dialog.Title>Confirm Action</Dialog.Title>
+                        <Dialog.Content>
+                            <Paragraph>Deleting will remove all records. Confirm to proceed.</Paragraph>
+                        </Dialog.Content>
+                        <Dialog.Actions>
+                            <Button onPress={() => handleDelete()}>CONFIRM</Button>
+                            <Button onPress={()=>{this.setState({showDialog : false})}}>CANCEL</Button>
+                        </Dialog.Actions>
+                    </Dialog>
+                </Portal>
             </ScrollView>
         )
     }
@@ -59,8 +71,8 @@ class RemoveBill extends React.Component{
 }
 
 const styles = StyleSheet.create({
-    card : {
-        margin : 10
+    card: {
+        margin: 10
     }
 })
 
